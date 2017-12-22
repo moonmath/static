6403dcb1ba83301c3e61d70f073d6d3fef082658
jarederaj <jarederaj@moonmath.win>
2017-12-22 11:51:08 -0700

    added 2017 rainbow chart and refactored javascript for multiple charts

--- js/Rainbows.jquery.js
+++ js/Rainbows.jquery.js
@@ -9,25 +9,52 @@ $(function ($) {
 
     $.Rainbows = $.extend($.Rainbows || {},  {
 
+        _elems: {},
+        horizontalGridLines: {},
+
         __init__: function () {
+            var count = -1;
+            while (count < 6) {
+                var cString = count.toString();
+                $.Rainbows.horizontalGridLines['gl' +cString]  = Math.pow(10, count);
+                $.Rainbows.horizontalGridLines['gl' +cString + '_25']  = Math.pow(10, (count + 0.25));
+                $.Rainbows.horizontalGridLines['gl' +cString + '_50']  = Math.pow(10, (count + 0.5));
+                $.Rainbows.horizontalGridLines['gl' +cString + '_75']  = Math.pow(10, (count + 0.75));
+                count += 1;
+            }
             $.Rainbows.timeCache = new Date(Date.now()).toISOString().split(":")[0];
-            $.Rainbows.sectors();
+            $.Rainbows.set(
+                'svg#r2017',
+                "data/rainbow2017.csv",
+                12,
+                "#mm-calcs table tbody tr:nth-child(7) td:nth-child(6)"
+            );
+            $.Rainbows.set(
+                'svg#r2012',
+                "data/rainbowVars.csv",
+                8,
+                "#mm-calcs table tbody tr:nth-child(7) td:nth-child(11)"
+            );
         },
 
-        sectors: function () {
-            var ctx = $.Rainbows;
-            ctx.svg = d3.select("svg#sectors");
+        set: function (target, source, strokeWidth, cdprString) {
+            $.Rainbows._elems[target] = {
+                strokeWidth: strokeWidth,
+                source: source,
+                svg: d3.select(target),
+                cdprString: cdprString
+            };
+            var slf = $.Rainbows._elems[target];
             var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                height = +ctx.svg.attr("height") - margin.top - margin.bottom;
-            ctx.g = ctx.svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            ctx.width = +ctx.svg.attr("width") - margin.left - margin.right;
+                height = +slf.svg.attr("height") - margin.top - margin.bottom;
+            slf.g = slf.svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            slf.width = +slf.svg.attr("width") - margin.left - margin.right;
             var parseTime = d3.timeParse("%d-%b-%y");
-            ctx.x = d3.scaleTime()
-                .rangeRound([0, ctx.width]);
-            ctx.y = d3.scaleLog()
+            slf.x = d3.scaleTime()
+                .rangeRound([0, slf.width]);
+            slf.y = d3.scaleLog()
                 .rangeRound([height, 0]);
-
-            d3.csv("data/rainbowVars.csv?v=" + ctx.timeCache, function (d, i, columns) {
+            d3.csv(source + "?v=" + slf.timeCache, function (d, i, columns) {
                 var date = Date.parse(d.date),
                     lightGreen = parseFloat(d.lightGreen),
                     lightOrange = parseFloat(d.lightOrange),
@@ -50,80 +77,71 @@ $(function ($) {
                 d.violet = (typeof violet === 'number') ? violet : d.violet;
                 d.yellow = (typeof yellow === 'number') ? yellow : d.yellow;
                 d.uv = (typeof uv === 'number') ? uv : d.uv;
-                count = -1;
-                while (count < 6) {
-                    var cString = count.toString();
-                    d['gl' +cString]  = Math.pow(10, count);
-                    d['gl' +cString + '_25']  = Math.pow(10, (count + 0.25));
-                    d['gl' +cString + '_50']  = Math.pow(10, (count + 0.5));
-                    d['gl' +cString + '_75']  = Math.pow(10, (count + 0.75));
-                    count += 1;
-                }
+                d = $.extend(d, $.Rainbows.horizontalGridLines);
                 return d;
             }, function (error, data) {
-                ctx.price();
+                $.Rainbows.setPrice(slf);
                 if (error) throw error;
-                ctx.x.domain(d3.extent(data, function (d) {
+                slf.x.domain(d3.extent(data, function (d) {
                     return d.dateNum;
                 }));
-                ctx.y.domain(d3.extent(data, function (d) {
+                slf.y.domain(d3.extent(data, function (d) {
                     return d.violet;
                 }));
                 count = -1;
                 while (count < 6) {
                     var cString = count.toString();
-                    ctx.gridLine('gl' + cString, data, 0.5);
-                    ctx.gridLine('gl' + cString + '_25', data, 0.2);
-                    ctx.gridLine('gl' + cString + '_50', data, 0.2);
-                    ctx.gridLine('gl' + cString + '_75', data, 0.2);
+                    $.Rainbows.setGridLine(slf,'gl' + cString, data, 0.5);
+                    $.Rainbows.setGridLine(slf,'gl' + cString + '_25', data, 0.2);
+                    $.Rainbows.setGridLine(slf,'gl' + cString + '_50', data, 0.2);
+                    $.Rainbows.setGridLine(slf,'gl' + cString + '_75', data, 0.2);
                     count += 1;
                 }
-                ctx.sectorColoring('yellow', '#F6FF00', data)
-                ctx.sectorColoring('lightGreen', '#86D40D', data)
-                ctx.sectorColoring('green', '#17BE01', data)
-                ctx.sectorColoring('red', '#FF008A', data)
-                ctx.sectorColoring('lightRed', '#FF0000', data)
-                ctx.sectorColoring('violet', '#0606CB', data)
-                ctx.sectorColoring('blue', '#0078EF', data)
-                ctx.sectorColoring('orange', '#F74A28', data)
-                ctx.sectorColoring('lightOrange', '#FEA417', data)
-                ctx.g.append("g")
+                $.Rainbows.setColors(slf, 'yellow', '#F6FF00', data)
+                $.Rainbows.setColors(slf, 'lightGreen', '#86D40D', data)
+                $.Rainbows.setColors(slf, 'green', '#17BE01', data)
+                $.Rainbows.setColors(slf, 'red', '#FF008A', data)
+                $.Rainbows.setColors(slf, 'lightRed', '#FF0000', data)
+                $.Rainbows.setColors(slf, 'violet', '#0606CB', data)
+                $.Rainbows.setColors(slf, 'blue', '#0078EF', data)
+                $.Rainbows.setColors(slf, 'orange', '#F74A28', data)
+                $.Rainbows.setColors(slf, 'lightOrange', '#FEA417', data)
+                slf.g.append("g")
                     .attr("transform", "translate(0," + height + ")")
                     .attr("class", "grid")
                     .call(
-                        d3.axisBottom(ctx.x)
-                        .tickSize(-ctx.width)
+                        d3.axisBottom(slf.x)
+                        .tickSize(-slf.width)
                     )
-                    .select(".domain")
-                    .remove();
-                ctx.g.append("g")
+                    .select(".domain");
+                slf.g.append("g")
                     .call(
-                        d3.axisLeft(ctx.y)
+                        d3.axisLeft(slf.y)
                     )
                     .append("text")
                     .attr("fill", "#000")
-                    .attr("transform", "rotate(-90)")
+//                    .attr("transform", "rotate(-90)")
                     .attr("y", 6)
-                    .attr("dy", "0.71em")
+                    .attr("dy", "-1em")
+                    .attr("dx", "20em")
                     .attr("text-anchor", "end")
-                    .text("Price ($)");
+                    .text("Price (USD) cast over CDPR: " + $(cdprString).text());
             });
+
 
         },
 
-        price: function () {
-            var ctx = $.Rainbows;
-            d3.csv("data/allData.csv?v=" + ctx.timeCache, function(d, i, columns) {
+        setPrice: function (slf) {
+            d3.csv("data/allData.csv?v=" + $.Rainbows.timeCache, function(d, i, columns) {
               d.Date = Date.parse(d.Date);
               d.Close = parseFloat(d.Close);
               return d;
             }, function(error, data) {
                 if (error) throw error;
-                var ctx = $.Rainbows;
                 var line = d3.line()
-                    .x(function(d) { return ctx.x(d.Date); })
-                    .y(function(d) { return ctx.y(d.Close); });
-                ctx.g.append("path")
+                    .x(function(d) { return slf.x(d.Date); })
+                    .y(function(d) { return slf.y(d.Close); });
+                slf.g.append("path")
                     .datum(data)
                     .attr("fill", "none")
                     .attr("stroke", "black")
@@ -140,10 +158,9 @@ $(function ($) {
         handleMouseOver: function(d, i, n) {
         },
 
-        gridLine: function (lineName, data, width) {
-            var ctx = $.Rainbows;
+        setGridLine: function (slf, lineName, data, width) {
             try {
-                ctx.g.append("path")
+                slf.g.append("path")
                     .datum(data)
                     .attr("fill", "none")
                     .attr("stroke", "black")
@@ -155,53 +172,50 @@ $(function ($) {
                         d3.line()
                         .x(function (d) {
                             try {
-                                return $.Rainbows.x(d.dateNum);
+                                return slf.x(d.dateNum);
                             } catch (err) { }
                         })
                         .y(function (d) {
                             try {
-                                return $.Rainbows.y(d[lineName]);
+                                return slf.y(d[lineName]);
                             } catch (err) { }
                         })
                         .curve(d3.curveCatmullRom.alpha(0))
                     )
-                    .on("mouseover", ctx.handleMouseOver)
-                    .on("mouseout", ctx.handleMouseOut);
+                    .on("mouseover", $.Rainbows.handleMouseOver)
+                    .on("mouseout", $.Rainbows.handleMouseOut);
             } catch (err) {
                 var foo = "bar"
             }
         },
 
-
-        sectorColoring: function (colorName, color, data) {
-            var ctx = $.Rainbows;
+        setColors: function (slf, colorName, color, data) {
             try {
-                ctx.g.append("path")
+                slf.g.append("path")
                     .datum(data)
                     .attr("class", color)
                     .attr("fill", "none")
                     .attr("stroke", color)
                     .attr("stroke-opacity", 0.3)
-                    .attr("stroke-width", 11)
+                    .attr("stroke-width", slf.strokeWidth)
                     .attr("d",
                         d3.line()
                         .x(function (d) {
                             try {
-                                return $.Rainbows.x(d.dateNum);
+                                return slf.x(d.dateNum);
                             } catch (err) { }
                         })
                         .y(function (d) {
                             try {
-                                return $.Rainbows.y(d[colorName]);
+                                return slf.y(d[colorName]);
                             } catch (err) { }
                         })
                         .curve(d3.curveCatmullRom.alpha(0))
                     )
-                    .on("mouseover", ctx.handleMouseOver)
-                    .on("mouseout", ctx.handleMouseOut);
+                    .on("mouseover", $.Rainbows.handleMouseOver)
+                    .on("mouseout", $.Rainbows.handleMouseOut);
             } catch(err) { }
-        },
-
+        }
     });
 
     var ctx = $.Rainbows;
